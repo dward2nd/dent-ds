37ba987c97b5e9b8968ecc497b84a8610bf91817
Saksinkarn Petchkuljinda <saksinkarn.cm@traditionasia.com>
2024-04-21 02:11:29 +0700

    Make the testcase more fuzzy to illustrate if our `print_override` still works

--- jury/test_print_override.py
+++ jury/test_print_override.py
@@ -1,6 +1,15 @@
 def caller_student():
     print("    Hello World!    ", "I'm so happy!", sep="\n")
+    print()
+    print("I called print many times.  ")
+    print()
+    print()
+    print("Sometimes", "just ", "empty lines", ".", sep="| ")
+    print("B", "U", "t", sep="~", end="55555")
+    print("It", "still", "works", "!!!", sep=",", end="\nkkk\n")
+    print("bye!!!\n\n")
 
 
 def caller_jury():
     print("  Hello Venus!    ")
+    print("I am Jury!")
